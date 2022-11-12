d975cc5835dd1b06050816a936f6ecfe813ec446
ananthu <ananthus.ann@gmail.com>
2022-11-12 21:45:55 +0530

    login form almost done

--- routes/user.js
+++ routes/user.js
@@ -3,21 +3,12 @@ var router = express.Router();
 const MongoClient=require('mongodb').MongoClient
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  product=[{ 
-    name:"Nokia",
-    price:6000,
-    discription:"This is a good product ",
-    image:"https://cdn.thewirecutter.com/wp-content/media/2021/08/budget-android-phone-2048px-nord-front.jpg"
-  },
-  {
-    name:"andriond",
-    price:1000,
-    discription:"This is a good product ",
-    image:"https://cdn.thewirecutter.com/wp-content/media/2021/08/budget-android-phone-2048px-nord-front.jpg"
-  }]
-  
-  res.render('user/index', {product,admin:false});
+router.get('/', function(req, res, next) {  
+  res.render('user/index', {admin:false});
 });
+
+router.get('/account',(req,res)=>{
+  res.render('user/account')
+})
 
 module.exports = router;
