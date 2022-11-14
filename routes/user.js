750baf777171dbdd5725aa765ae8498234cf234c
ananthu <ananthus.ann@gmail.com>
2022-11-14 11:33:39 +0530

    product grid aspect ration completed

--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,26 @@ var express = require('express');
 var router = express.Router();
 const MongoClient=require('mongodb').MongoClient
 
-/* GET home page. */
 router.get('/', function(req, res, next) {  
   res.render('user/index', {admin:false});
 });
 
-router.get('/account',(req,res)=>{
-  res.render('user/account')
+router.get('/login',(req,res)=>{
+  res.render('user/login')
 })
+
+router.post('/login',(req,res)=>{
+  console.log(req.body)
+})
+
+router.get('/sign-up',(req,res)=>{
+  res.render('user/sign-up')
+})
+
+router.post('/sign-up',(req,res)=>{
+  console.log(req.body)
+})
+
+
 
 module.exports = router;
